1d9116268c39ceef5b430df5a3bd1d33a282fb9c
james2doyle <james2doyle@gmail.com>
2013-05-27 17:26:07 -0400

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    imagemin: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'img',
+          src: '{,*/}*.{png,jpg,jpeg}',
+          dest: 'img'
+        }]
+      }
+    },
+    cssmin: {
+      add_banner: {
+        options: {
+          banner: '/*! <%= pkg.name %> v<%= pkg.version %> ' + '<%= grunt.template.today("mm.dd.yyyy") %> */'
+        },
+        files: {
+          'css/style.min.css': ['css/**/*.css']
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> v<%= pkg.version %> ' + '<%= grunt.template.today("mm.dd.yyyy") %> */'
+      },
+      my_target: {
+        files: {
+          'js/script.min.js': ['js/**/*.js']
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['imagemin', 'cssmin', 'uglify']);
+};
