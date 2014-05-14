aae4d545af72457b7a8c855b693e0f1713baa1ca
james2doyle <james2doyle@gmail.com>
2014-05-14 16:55:02 -0400

    remove modernizr svg fallback code. run grunt build

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,1 @@
 console.log('js loaded');
-if (!Modernizr.svg) {
-  (function() {
-    var imgs = document.getElementsByTagName('img'),endsWithDotSvg = /.*\.svg$/,i = 0,l = imgs.length;
-    // quick for loop
-    for(; i < l; ++i) {
-      if(imgs[i].src.match(endsWithDotSvg)) {
-        imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
-      }
-    }
-  })();
-}
